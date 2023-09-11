59b31d04a93972a0d0ccf2e2227be4380caf6ac2
Vitalii <vel.kun4666@gmail.com>
2023-09-11 15:23:24 +0300

    Completed homework number 2

---
+++ src/main/java/Homewok_nr_2/HomeWork2.java
@@ -0,0 +1,28 @@
+package Homewok_nr_2;
+
+public class HomeWork2 {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        char FirstLetterInAlphabet = 'A';
+        boolean JavaIsAmazing = true;
+        float Pi = 3.14159f;
+
+        System.out.println("CurrentWorldPopulation" + CurrentWorldPopulation);
+        System.out.println("CarsProducedThisYear: " + CarsProducedThisYear);
+        System.out.println("FirstLetterInAlphabet: " + FirstLetterInAlphabet);
+        System.out.println("JavaIsAmazing: " + JavaIsAmazing);
+        System.out.println("Pi: " + Pi);
+
+        double price = 99.9999999999999999D;
+        double taxi = 89.57674629069270001D;
+        int quantity = 13;
+        double sum = -12;
+        double total =  price + taxi * quantity + sum;
+
+        System.out.println("\nTotal: " + total);
+
+    }
+}
